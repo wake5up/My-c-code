b6b26b9f9a08ef832f8ae45cdbda2916e844d0d8
郝团团 <2740276135@qq.com>
2019-07-23 20:22:24 +0800

    循环队列，队列的插删功能和查头查尾功能

---
+++ round-robin queue/queue.h
@@ -0,0 +1,24 @@
+#ifndef _QUEUE_H_
+#define _QIEIE_H_
+
+#include<stdio.h>
+
+#define QueueMAX 5
+typedef int QuDataType;
+typedef struct Queue
+{
+	QuDataType data[QueueMAX];
+	QuDataType *front;
+	QuDataType *rear;
+	size_t size;
+}Queue;
+
+void QueueInit(Queue* pq); 
+void QueueDestory(Queue* pq);
+
+void QueuePush(Queue* pq, QuDataType x);
+void QueuePop(Queue* pq); QuDataType QueueFront(Queue* pq);
+QuDataType QueueFront(Queue* pq);
+QuDataType QueueBack(Queue* pq); 
+
+#endif
