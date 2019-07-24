cf80920bebeb50769c38ceed82231770d6299d2c
郝团团 <2740276135@qq.com>
2019-07-24 20:37:05 +0800

    二叉树的创建

---
+++ binary tree/BTree.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "BTree.h"
+
+BTNode* BinaryTreeCreate(BTDataType* a)
+{
+	BTNode * cur;
+	static int i = 0;
+
+	if (a[i] == ENDTAG)
+	{
+		i++;
+		return NULL;
+	}
+	cur = (BTNode *)malloc(sizeof(BTNode));
+	cur->_data = a[i];
+	i++;
+	cur->_left = BinaryTreeCreate(a);
+	cur->_right = BinaryTreeCreate(a);
+
+	return cur;
+}
