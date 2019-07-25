e1caecfd4bbd6c79da02d1103f84d6994968cf96
郝团团 <2740276135@qq.com>
2019-07-25 22:24:15 +0800

    二叉树的创建，前序，中序，后序的实现，以及运用队列实现层序

--- binary tree/BTree.c
+++ binary tree/BTree.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include "BTree.h"
+#include"queue.h"
 
 BTNode* BinaryTreeCreate(BTDataType* a)
 {
@@ -17,6 +18,60 @@ BTNode* BinaryTreeCreate(BTDataType* a)
 	i++;
 	cur->_left = BinaryTreeCreate(a);
 	cur->_right = BinaryTreeCreate(a);
-
 	return cur;
 }
+void BinaryTreePrevOrder(BTNode* root)//前序
+{
+	if (root != NULL)
+	{
+		printf("%c",root->_data);
+		BinaryTreePrevOrder(root->_left);
+		BinaryTreePrevOrder(root->_right);
+	}	
+}
+void BinaryTreeInOrder(BTNode* root)//中序
+{
+	if (root != NULL)
+	{
+		BinaryTreePrevOrder(root->_left);
+		printf("%c", root->_data);
+		BinaryTreePrevOrder(root->_right);
+	}
+}
+void BinaryTreePostOrder(BTNode* root)//后序
+{
+	if (root != NULL)
+	{
+		BinaryTreePrevOrder(root->_left);
+		BinaryTreePrevOrder(root->_right);
+		printf("%c", root->_data);
+	}
+}
+
+void BinaryTreeLevelOrder(BTNode* root)//层序——》广度遍历排序
+{
+	Queue qu;
+	BTNode * tmp;
+	QueueInit(&qu);
+	QueuePush(&qu, root);
+	while (!QueueIsEmpty(&qu))
+	{
+		tmp = QueueFront(&qu);
+		printf("%c", tmp->_data);
+		if (tmp->_left)
+		{
+			QueuePush(&qu, tmp->_left);
+		}
+		if (tmp->_right)
+		{
+			QueuePush(&qu, tmp->_right);
+		}
+		QueuePop(&qu);
+	}
+	QueueDestory(&qu);
+}
+
+
+
+
+
--- binary tree/main.c
+++ binary tree/main.c
@@ -1,1 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include"BTree.h"
+
+int main()
+{
+	BTNode * testTree = BinaryTreeCreate("ABDF####CE#GH##I#J###");
+
+	BinaryTreePrevOrder(testTree);
+	putchar('\n');
+	BinaryTreeInOrder(testTree);
+	putchar('\n');
+	BinaryTreePostOrder(testTree);
+	putchar('\n');
+	BinaryTreeLevelOrder(testTree);
+	putchar('\n');
+	return 0;
+}
+
+//汉诺塔问题
+//void move(char pos1, char pos2)//移动步骤函数
+//{
+//	printf("%c-->%c\n", pos1, pos2);
+//}
+//void Hanoi(int n, char pos1, char pos2, char pos3)
+//{
+//	if (n == 1)
+//	{
+//		move(pos1, pos3);//递归出口
+//	}
+//	else
+//	{
+//		Hanoi(n - 1, pos1, pos3, pos2);
+//		move(pos1, pos3);
+//		Hanoi(n - 1, pos2, pos1, pos3);
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	printf("请输入汉诺塔盘子数:");
+//	scanf("%d", &n);
+//	Hanoi(n, 'A', 'B', 'C');
+//	return 0;
+//}
